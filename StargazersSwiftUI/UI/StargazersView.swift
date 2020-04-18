d985935d3e45bb67d164031cc2affcb6f84ea373
gualtierofrigerio <gualtiero.frigerio@gmail.com>
2020-04-18 16:17:54 +0200

    added infitine scroll

--- StargazersSwiftUI/UI/StargazersView.swift
+++ StargazersSwiftUI/UI/StargazersView.swift
@@ -6,20 +6,52 @@
 //  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
 //
 
+import Combine
 import SwiftUI
 
 struct StargazersView: View {
-    var stargazers:[User] = []
+    @ObservedObject var viewModel:StargazersViewModel
     
     var body: some View {
-        List(stargazers) { stargazer in
+        VStack {
+            List(viewModel.stargazers) { stargazer in
+                StargazerView(stargazer: stargazer)
+            }
+            Button(action: {
+                self.viewModel.getNextStargazers { success in
+                    print("next data received")
+                }
+            })
+            {
+                Text("next")
+            }
+        }
+    }
+}
+
+struct StargazersViewInfiniteScroll: View {
+    @ObservedObject var viewModel:StargazersViewModel
+    
+    var body: some View {
+        List(viewModel.stargazers) { stargazer in
             StargazerView(stargazer: stargazer)
+                .onAppear {
+                    self.elementOnAppear(stargazer)
+            }
+        }
+    }
+    
+    private func elementOnAppear(_ stargazer:User) {
+        if self.viewModel.isLastStargazer(stargazer) {
+            self.viewModel.getNextStargazers { success in
+                print("next data received")
+            }
         }
     }
 }
 
 struct StargazersView_Previews: PreviewProvider {
     static var previews: some View {
-        StargazersView()
+        StargazersView(viewModel: StargazersViewModel())
     }
 }
