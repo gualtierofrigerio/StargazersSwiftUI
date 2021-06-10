ada7517a2d0a6f1e189b7023307d9a26f06e2d6e
Gualtiero <gualtiero.frigerio@gmail.com>
2021-06-10 09:42:12 +0200

    AsyncImage with phase

--- StargazersSwiftUI/UI/StargazerView.swift
+++ StargazersSwiftUI/UI/StargazerView.swift
@@ -14,19 +14,36 @@ struct StargazerView: View {
     var body: some View {
         HStack {
             if #available(iOS 15.0, *) {
-                AsyncImage(url: URL(string: stargazer.avatarUrl)!) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                } placeholder: {
-                    ProgressView()
+                AsyncImage(url: URL(string: stargazer.avatarUrl)!) { phase in
+                    viewForPhase(phase)
+                        .frame(width: 100, height: 100)
                 }
-                .frame(width: 100, height: 100)
             }
             else {
                 ImageView(withURL: stargazer.avatarUrl)
             }
             Text(stargazer.login)
+        }
+    }
+    
+    @available(iOS 15.0, *)
+    @ViewBuilder
+    private func viewForPhase(_ phase:AsyncImagePhase) -> some View {
+        switch phase {
+        case .empty:
+            ProgressView()
+        case .success(let image):
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        case .failure(let error):
+            let _ = print("error \(error)")
+            Image(systemName: "person.fill.xmark")
+                .font(.largeTitle)
+        @unknown default:
+            let _ = print("unknown phase \(phase)")
+            Image(systemName: "person.fill.xmark")
+                .font(.largeTitle)
         }
     }
 }
